91c2906635d863afa6c056861626cef8de22d9ed
Haerul Taka <haerultaka255@gmail.com>
2023-10-20 23:15:48 +0700

    add static data to mongoDB

---
+++ models/ticket.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = mongoose.Schema(
+  {
+    nameOfShip: {
+      type: String,
+      require: true,
+    },
+    classOfDeparture: {
+      type: String,
+      require: true,
+    },
+
+    vehicleType: {
+      type: String,
+      require: true,
+    },
+    ticketPrice: {
+      type: Number,
+      require: true,
+    },
+    route: {
+      type: String,
+      require: true,
+    },
+
+    currentBooks: {},
+    description: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ticketModels = mongoose.Model("ticket", ticketSchema);
+
+module.exports = ticketModels;
