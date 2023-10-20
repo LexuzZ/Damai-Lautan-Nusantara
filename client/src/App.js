42cbec0e270870a4b30d891af87a4d8daf6579a6
Haerul Taka <haerultaka255@gmail.com>
2023-10-21 00:43:17 +0700

    get all data front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Homescreen from "./screen/Homescreen";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" exact element={<Homescreen />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/screen/Homescreen.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Homescreen() {
+  return (
+    <div>
+      <h1>Home Screen</h1>
+    </div>
+  );
+}
+
+export default Homescreen;
